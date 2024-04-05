462ab4b1546ea86555fd8fa2ed0d69ef4567850c
PradanaB4CKUP <aji2207ok@gmail.com>
2024-04-05 21:13:07 +0700

    halaman 73

--- application/views/autentifikasi/login.php
+++ application/views/autentifikasi/login.php
@@ -35,12 +35,10 @@
                                 </form>
                                 <hr>
                                 <div class="text-center">
-                                    <a class="small" href="<?=
-                                        base_url('autentifikasi/lupaPassword'); ?>">Lupa Password?</a>
+                                    <a class="small" href="<?= base_url('autentifikasi/lupaPassword'); ?>">Lupa Password?</a>
                                 </div>
                                 <div class="text-center">
-                                    <a class="small" href="<?=
-                                        base_url('autentifikasi/registrasi'); ?>">Daftar Member!</a>
+                                    <a class="small" href="<?= base_url('autentifikasi/registrasi'); ?>">Daftar Member!</a>
                                 </div>
                             </div>
                         </div>
